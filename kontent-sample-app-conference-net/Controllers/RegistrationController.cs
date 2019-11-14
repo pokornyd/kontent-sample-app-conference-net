1b80fe60dc151a533bc7794693bce30da3befa13
MichaelB <MichaelB@kentico.com>
2019-11-14 08:35:53 -0500

    Adds feature #9 and fixes #8

---
+++ kontent-sample-app-conference-net/Controllers/RegistrationController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Kentico.Kontent.Delivery;
+using KenticoKontentModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace kontent_sample_app_conference_net.Controllers
+{
+    public class RegistrationController : BaseController
+    {
+        public RegistrationController(IDeliveryClient deliveryClient) : base(deliveryClient)
+        {
+
+        }
+
+        public async Task<ViewResult> Index(string location)
+        {
+            ViewBag.location = location;
+
+            DeliveryItemListingResponse<Registration> response = await DeliveryClient.GetItemsAsync<Registration>(
+                new EqualsFilter("system.type", "registration")
+                );
+            return View(response.Items[0]);
+        }
+    }
+}
