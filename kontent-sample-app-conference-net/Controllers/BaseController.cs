9e3d70593256a11912160e44c4bc4e8e08d96029
Daniel Pokorny <52500882+pokornyd@users.noreply.github.com>
2020-01-07 10:32:07 +0100

    Update BaseController.cs

--- kontent-sample-app-conference-net/Controllers/BaseController.cs
+++ kontent-sample-app-conference-net/Controllers/BaseController.cs
@@ -29,7 +29,7 @@ namespace kontent_sample_app_conference_net.Controllers
 
             if (HttpContext.Request.Host.ToString() == "konnections.herokuapp.com")
             {
-                result = "db96e910-edf8-0094-b795-f3ce073c7ae0";
+                result = "test";//"db96e910-edf8-0094-b795-f3ce073c7ae0";
             }
 
             else
